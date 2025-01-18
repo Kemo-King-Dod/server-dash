4f2548396fc722e29e6f848f0d30c68902a2952e
Kemo-King-Dod <kamalmrwan6@gmail.com>
2025-01-18 20:48:57 +0200

    edit13

--- server.js
+++ server.js
@@ -19,10 +19,10 @@ const io = createserver(expressserver);
 io.on("connection", connect);
 
 // Error handling middleware (should be last)
-const errorHandler = require('./middleware/ErrorHandler');
-app.use(errorHandler.AppError);
-app.use(errorHandler.catchAsync);
-app.use(errorHandler.errorHandler);
+// const errorHandler = require('./middleware/ErrorHandler');
+// app.use(errorHandler.AppError);
+// app.use(errorHandler.catchAsync);
+// app.use(errorHandler.errorHandler);
 
 const loadphoto = require("./routes/loadPhoto.js");
 app.use(loadphoto);
