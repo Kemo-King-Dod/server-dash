7040af9ec4718171198635776e8ea8a151ac14c8
Kemo-King-Dod <kamalmrwan6@gmail.com>
2025-02-27 16:36:57 +0200

    hello

--- routes/store.js
+++ routes/store.js
@@ -46,6 +46,7 @@ router.get('/getStores', auth, async (req, res) => {
             const user = await User.findOne({ _id: id });
             for (var i = 0; i < stores.length; i++) {
                 for (var j = 0; j < user.favorateStors.length; j++) {
+                    console.log(user.favorateStors[j])
                     if (user.favorateStors[j]._id == stores[i]._id) {
                         stores[i]._doc.isFavorite = true;
                     }
