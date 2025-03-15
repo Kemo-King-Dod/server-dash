6c3c90f5dbe25d14f4bc4b592b19f00863d95908
Kemo-King-Dod <kamalmrwan6@gmail.com>
2025-03-15 14:01:52 +0200

    chat

--- routes/chat.js
+++ routes/chat.js
@@ -20,10 +20,10 @@ route.get('/driverChat', auth, async (req, res) => {
         })
     }
 })
-route.get('/storeChat', auth, async (req, res) => {
+route.get('/userChat', auth, async (req, res) => {
     try {
         const id = req.userId
-        const order = await Order.findOne({ 'store.id': id })
+        const order = await Order.findOne({ 'customer.id': id })
         res.status(200).json({
             error: false,
             data: order.chat
@@ -82,7 +82,7 @@ route.post('/driverSendMassege', auth, async (req, res) => {
     }
 });
 
-route.post('/storeSendMassege', auth, async (req, res) => {
+route.post('/userSendMassege', auth, async (req, res) => {
     try {
         const id = req.body.id
         const { message } = req.body.message;
@@ -99,7 +99,7 @@ route.post('/storeSendMassege', auth, async (req, res) => {
 
         // Create a new message object
         const newMessage = {
-            sender: 'store',
+            sender: 'user',
             content: message,
             timestamp: new Date()
         };
