eb3ab2af7715127ce48934d1edea9b030c1ad2f4
Kemo-King-Dod <kamalmrwan6@gmail.com>
2025-03-17 16:48:34 +0200

    discount

--- routes/retrenchments.js
+++ routes/retrenchments.js
@@ -1,22 +1,38 @@
 const express = require('express');
 const router = express.Router();
 const { auth } = require('../middleware/auth');
+const Item = require('../database/items');
 const Retrenchments = require('../database/Retrenchments');
 
-router.post('./retrenchments', auth, async (req, res) => {
-
+router.post('/discount', auth, async (req, res) => {
     try {
+        console.log(req.body)
         const userId = req.userId
-        const { name, items, retrenchment_percent, retrenchment_start, retrenchment_end } = req.body
+        const { name, applicableProducts, percent, startDate, endDate } = req.body
 
         await Retrenchments.create({
             name,
-            items,
+            items: applicableProducts,
             store_id: userId,
-            retrenchment_percent,
-            retrenchment_start,
-            retrenchment_end
+            retrenchment_percent: percent,
+            retrenchment_start: startDate,
+            retrenchment_end: endDate
         })
+
+        // make discount for items
+        await Item.updateMany(
+            {
+                _id: { $in: applicableProducts },
+                storeID: userId
+            },
+            {
+                $set: {
+                    is_retrenchment: true,
+                    retrenchment_percent: percent
+                }
+            }
+        );
+
         res.status(200).json({
             error: false,
             message: 'تم إضافة التخفيض بنجاح',
