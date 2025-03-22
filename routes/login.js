2d20af390f238287a8e572fb6eb5c6f9b3db6053
Kemo-King-Dod <kamalmrwan6@gmail.com>
2025-03-22 16:49:28 +0200

    is phone exist

--- routes/login.js
+++ routes/login.js
@@ -104,6 +104,7 @@ route.post("/isPhoneExist", async (req, res) => {
         if (!user) {
             return res.status(404).json({
                 error: true,
+                isExist: false,
                 data: "رقم الهاتف غير موجود"
             });
         }
