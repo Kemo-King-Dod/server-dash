6f810a8bbb91eb9a6ec72a78c2f6c42a9dc790f6
Kemo-King-Dod <kamalmrwan6@gmail.com>
2025-05-22 03:28:43 +0200

    test

--- database/database.js
+++ database/database.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose')
 
 const connecting = async () => {
     try{
-        await mongoose.connect('mongodb+srv://abdelrhamn98:0922224420@cluster0.7dk3i.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0')
+        await mongoose.connect("mongodb+srv://murafiq00:123456%402025@murafiq.lmuoyzo.mongodb.net/fasto?retryWrites=true&w=majority&appName=Murafiq");
         console.log('connection succuss')
     }catch(err){
         console.log(err)
