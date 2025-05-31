b3d6edaf28a65830e33b30c55397917aee0817c4
Kemo-King-Dod <kamalmrwan6@gmail.com>
2025-06-01 00:44:26 +0200

    test

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,44 +1,43 @@
-const jwt = require('jsonwebtoken');
-const User = require('../database/users');
-const Driver = require('../database/driver');
-const Store = require('../database/store');
-const Admin = require('../database/admin');
+const jwt = require("jsonwebtoken");
+const User = require("../database/users");
+const Driver = require("../database/driver");
+const Store = require("../database/store");
+const Admin = require("../database/admin");
 
 const JWT_SECRET = "Our_Electronic_app_In_#Sebha2024_Kamal_&_Sliman";
 
 const auth = async (req, res, next) => {
-    try {
-        console.log(req.headers)
-        const token = req.header('Authorization')?.replace('Bearer ', '');
-        if (!token) {
-            return res.status(401).json({
-                error: true,
-                message: 'يرجى تسجيل الدخول'
-            });
-        }
-
-        const decoded = await jwt.verify(token, JWT_SECRET)
-     
-
-        let exist = await User.findOne({ _id: decoded.id });
-        if (!exist) exist = await Driver.findOne({ _id: decoded.id })
-        if (!exist) exist = await Store.findOne({ _id: decoded.id })
-
-        // Find user across all collections
-        if (!exist)exist= await Admin.findOne({ _id: decoded.id })
-        exist.fcmToken = req.headers['fcm_token']
-        exist.save()
-        req.userId = decoded.id;
-        req.user = exist;
-
-        next();
-    } catch (error) {
-        console.error(error);
-        res.status(401).json({
-            error: true,
-            message: 'يرجى الدخول'
-        });
+  try {
+    console.log(req.headers);
+    const token = req.header("Authorization")?.replace("Bearer ", "");
+    if (!token) {
+      return res.status(401).json({
+        error: true,
+        message: "يرجى تسجيل الدخول",
+      });
     }
+
+    const decoded = await jwt.verify(token, JWT_SECRET);
+
+    let exist = await User.findOne({ _id: decoded.id });
+    if (!exist) exist = await Driver.findOne({ _id: decoded.id });
+    if (!exist) exist = await Store.findOne({ _id: decoded.id });
+
+    // Find user across all collections
+    if (!exist) exist = await Admin.findOne({ _id: decoded.id });
+    exist.fcmToken = req.headers["fcm_token"];
+    await exist.save();
+    req.userId = decoded.id;
+    req.user = exist;
+
+    next();
+  } catch (error) {
+    console.error(error);
+    res.status(401).json({
+      error: true,
+      message: "يرجى الدخول",
+    });
+  }
 };
 
 module.exports = { auth };
