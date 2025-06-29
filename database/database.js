92d80d408da1aeb8cce96a3e1d43551dad18e78a
Kemo-King-Dod <kamalmrwan6@gmail.com>
2025-06-29 17:29:40 +0200

    test

--- database/database.js
+++ database/database.js
@@ -1,13 +1,23 @@
 const mongoose = require('mongoose')
 
 const connecting = async () => {
-    try{
-        await mongoose.connect("mongodb+srv://murafiq00:123456%402025@murafiq.lmuoyzo.mongodb.net/fasto?retryWrites=true&w=majority&appName=Murafiq");
-        console.log('connection succuss')
-    }catch(err){
-        console.log(err)
-    }
-}
+  try {
+    await mongoose.connect(
+      "mongodb+srv://murafiq00:123456%402025@murafiq.lmuoyzo.mongodb.net/fasto?retryWrites=true&w=majority&appName=Murafiq"
+    );
+    console.log("✅ connection success");
+
+    // ‼️ احصل على buildInfo من الخادم
+    const info = await mongoose.connection.db
+      .admin()
+      .command({ buildInfo: 1 });
+
+    console.log("🛈 MongoDB server version →", info.version); // مثل 8.1.1
+  } catch (err) {
+    console.error("❌ connection failed");
+    console.error(err);
+  }
+};
 
 module.exports = connecting
 
