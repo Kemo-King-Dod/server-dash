0914929637ddedf1aedb99f53b3a810fdb5a93f9
Kemo-King-Dod <kamalmrwan6@gmail.com>
2025-07-07 02:07:31 +0200

    test

--- routes/cart.js
+++ routes/cart.js
@@ -180,6 +180,7 @@ router.post("/addtocart", auth, async (req, res) => {
     }
     cartItem.isModfiy = store.isModfiy;
     cartItem.modfingPrice = store.modfingPrice;
+    cartItem.quantity =1;
 
     for (var i = 0; i < cartItem.options.length; i++) {
       if (cartItem.options[i].isSelected) {
