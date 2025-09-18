c15f123d34bfcca277c98526b9ec0602ee0eaceb
Kemo-King-Dod <kamalmrwan6@gmail.com>
2025-09-18 17:47:31 +0200

    test

--- routes/version.js
+++ routes/version.js
@@ -22,5 +22,15 @@ router.get('/getVersionForDriver', async (req, res) => {
     });
 });
 
+router.get('/getVersionForQueue', async (req, res) => {
+    var version = require('../utils/queueVersion.json');
+    version.image = path.join('uploads','pictures',version.image);
+    res.status(200).json({
+        error: false,
+        message: "Version fetched successfully",
+        data:version
+    });
+});
+
 module.exports = router;
 
