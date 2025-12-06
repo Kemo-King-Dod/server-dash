e57b90cc5b4cab8d3a9c0c5f3231dcf412b14486
Kemo-King-Dod <kamalmrwan6@gmail.com>
2025-12-06 02:36:08 +0200

    hello

--- routes/category_mangement.js
+++ routes/category_mangement.js
@@ -126,7 +126,7 @@ router.post('/deleteCategory', (req, res) => {
     })
   }
 })
-router.post('/updateCategory',upload.single('photo'), (req, res) => {
+router.patch('/updateCategory',upload.single('photo'), (req, res) => {
   try {
     if (!req.body.password || req.body.password !== 'Chackmate@9876') {
       return res.status(401).json({
