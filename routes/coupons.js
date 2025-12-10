239886c7ceececc2d86b48d563fe1512133b10c2
Kemo-King-Dod <136559933+Kemo-King-Dod@users.noreply.github.com>
2025-12-10 20:07:31 +0200

    rating

--- routes/coupons.js
+++ routes/coupons.js
@@ -1,7 +1,7 @@
 const express = require("express")
 const router = express.Router()
 
-router.post("/applycoupon", (req, res) => {
+router.post("/applycoupon", auth, (req, res) => {
 
     try {
         const { couponCode } = req.body;
